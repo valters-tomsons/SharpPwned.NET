b13e896ba5b9e8400743eff56038b0c6973d7e77
Valters <valters.tomsons@protonmail.com>
2021-06-12 14:21:12 +0300

    Clean up styling

--- SharpPwned.NET/HaveIBeenPwnedRestClient.cs
+++ SharpPwned.NET/HaveIBeenPwnedRestClient.cs
@@ -12,33 +12,34 @@ namespace SharpPwned.NET
 {
     public class HaveIBeenPwnedRestClient : IHaveIBeenPwnedRestClient
     {
-        private static readonly HttpClient client = new HttpClient();
-
         private readonly string URL = "https://haveibeenpwned.com/api/v3";
         private readonly string passwordRangeURL = "https://api.pwnedpasswords.com";
-        private readonly string userAgent;
-        private readonly string hibpApiKey;
+
+        private static readonly HttpClient _httpClient = new HttpClient();
+
+        private readonly string _userAgent;
+        private readonly string _apiKey;
 
         public HaveIBeenPwnedRestClient(string hibpApiKey, string userAgent = null)
         {
-            this.userAgent = userAgent;
-            this.hibpApiKey = hibpApiKey;
-            if (string.IsNullOrWhiteSpace(this.userAgent))
+            _userAgent = userAgent;
+            _apiKey = hibpApiKey;
+
+            if (string.IsNullOrWhiteSpace(userAgent))
             {
-                this.userAgent = "SharpPwned.NET";
+                _userAgent = "SharpPwned.NET";
             }
         }
 
         public async Task<List<Paste>> GetPasteAccount(string account)
         {
-            string api = "pasteaccount";
-            var response = await GETRequestAsync($"{api}/{account}");
+            const string api = "pasteaccount";
+            var response = await GETRequestAsync($"{api}/{account}").ConfigureAwait(false);
 
             if (response.StatusCode == "OK")
             {
                 string body = response.Body;
-                var allPastes = JsonConvert.DeserializeObject<List<Paste>>(body);
-                return allPastes;
+                return JsonConvert.DeserializeObject<List<Paste>>(body);
             }
 
             return null;
@@ -46,57 +47,50 @@ namespace SharpPwned.NET
 
         public async Task<Breach> GetBreach(string site)
         {
-            string api = "breach";
-            var response = await GETRequestAsync($"{api}/{site}");
+            const string api = "breach";
+            var response = await GETRequestAsync($"{api}/{site}").ConfigureAwait(false);
 
             if (response.StatusCode == "OK")
             {
                 string body = response.Body;
-                var breach = JsonConvert.DeserializeObject<Breach>(body);
-                return breach;
+                return JsonConvert.DeserializeObject<Breach>(body);
             }
 
             return null;
-
         }
 
         public async Task<List<Breach>> GetAllBreaches()
         {
-            string api = "breaches";
-            var response = await GETRequestAsync(api);
+            const string api = "breaches";
+            var response = await GETRequestAsync(api).ConfigureAwait(false);
 
-            List<Breach> AllBreaches = new List<Breach>();
             if (response.StatusCode == "OK")
             {
                 string body = response.Body;
-                AllBreaches = JsonConvert.DeserializeObject<List<Breach>>(body);
-                return AllBreaches;
+                return JsonConvert.DeserializeObject<List<Breach>>(body);
             }
 
-            return AllBreaches;
-
+            return new List<Breach>();
         }
 
         public async Task<List<Breach>> GetAccountBreaches(string account, bool? includeUnverified = true)
         {
-            string api = "breachedaccount";
+            const string api = "breachedaccount";
             string includeUnverifiedQueryString = string.Empty;
-            if(includeUnverified.HasValue && !includeUnverified.Value)
+
+            if(includeUnverified == false)
             {
                 includeUnverifiedQueryString = "?includeUnverified=false";
             }
-            var response = await GETRequestAsync($"{api}/{account}{includeUnverifiedQueryString}");
-
-            List<Breach> AllBreaches = new List<Breach>();
+            var response = await GETRequestAsync($"{api}/{account}{includeUnverifiedQueryString}").ConfigureAwait(false);
 
             if(response.StatusCode == "OK")
             {
                 string body = response.Body;
-                AllBreaches = JsonConvert.DeserializeObject<List<Breach>>(body);
-                return AllBreaches;
+                return JsonConvert.DeserializeObject<List<Breach>>(body);
             }
 
-            return AllBreaches;
+            return new List<Breach>();
         }
 
         public async Task<bool> IsPasswordPwned(string password)
@@ -105,32 +99,29 @@ namespace SharpPwned.NET
             SHA1 sha1 = SHA1.Create();
             byte[] byteString = Encoding.UTF8.GetBytes(password);
             byte[] hashBytes = sha1.ComputeHash(byteString);
-            string hashString = "";
+
             StringBuilder sb = new StringBuilder();
             foreach (byte b in hashBytes)
             {
                 sb.Append(b.ToString("X2"));
             }
-            hashString = sb.ToString();
 
+            string hashString = sb.ToString();
             // Break the SHA1 into two pieces:
             //   1) the first five characters of the hash
             //   2) the rest of the hash
             string hashFirstFive = hashString.Substring(0, 5);
             string hashLeftover = hashString.Substring(5, hashString.Length - 5);
 
-            string api = "range";
-            var response = await GETRequestAsync($"{api}/{hashFirstFive}", passwordRangeURL);
-            var responseContainsHash = response.Body.Contains(hashLeftover);
+            const string api = "range";
+            var response = await GETRequestAsync($"{api}/{hashFirstFive}", passwordRangeURL).ConfigureAwait(false);
 
-            return responseContainsHash;
-
+            return response.Body.Contains(hashLeftover);
         }
 
         private async Task<Response> GETRequestAsync(string parameters)
         {
-            Response response = await GETRequestAsync(parameters, URL);
-            return response;
+            return await GETRequestAsync(parameters, URL).ConfigureAwait(false);
         }
 
         private async Task<Response> GETRequestAsync(string parameters, string overrideURL)
@@ -139,17 +130,17 @@ namespace SharpPwned.NET
             Uri uri = new Uri($"{overrideURL}/{parameters}");
 
             HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, uri);
-            request.Headers.Add ("hibp-api-key", hibpApiKey);
+            request.Headers.Add ("hibp-api-key", _apiKey);
 
             HttpResponseMessage response = null;
-            request.Headers.TryAddWithoutValidation("User-Agent", userAgent);
+            request.Headers.TryAddWithoutValidation("User-Agent", _userAgent);
 
             try
             {
-                response = await client.SendAsync(request);
+                response = await _httpClient.SendAsync(request).ConfigureAwait(false);
                 response.EnsureSuccessStatusCode();
 
-                string responseBody = await response.Content.ReadAsStringAsync();
+                string responseBody = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
                 string statusCode = response.StatusCode.ToString();
 
                 RestResponse.Body = responseBody;
